ab161096fde80269ff83062b07f841d0cb929bc2
georgiysultanov <79409313+georgiysultanov@users.noreply.github.com>
2022-09-22 18:22:01 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+"""
+:authors: George Sultanov
+:license: See LICENSE file
+:copyright: (c) 2022 George Sultanov
+"""
+
+setup(name='fockspy',
+      version='0.1.11',
+      description='Python module for making calculations connected with open quantum systems',
+      packages=['fockspy'],
+      author_email='gfaberlane@gmail.com',
+      zip_safe=False)
+
